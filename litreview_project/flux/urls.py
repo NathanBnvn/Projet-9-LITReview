a82054e34f6030e50fe7cd6bdee6aff96e3d94dd
Nathan Bienvenu <bienvenu.nathan72@gmail.com>
2021-06-08 18:24:17 +0200

    création du projet 9

---
+++ litreview_project/flux/urls.py
@@ -0,0 +1,7 @@
+# from django.conf.urls import path
+# from . import views
+
+# urlpatterns = [
+# 	path('create-review', views.create_review, name='create-review')
+# 	path('ask-review, views.ask_review, name='ask-review')
+# ]
