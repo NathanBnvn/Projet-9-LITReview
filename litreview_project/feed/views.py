0f4c291d8dafdb499056170c0909413f5a3df33a
Nathan Bienvenu <bienvenu.nathan72@gmail.com>
2021-07-13 13:03:53 +0200

    amélioration du code et résolution du filtrage des boutons

--- litreview_project/feed/views.py
+++ litreview_project/feed/views.py
@@ -8,43 +8,76 @@ from .forms import TicketForm, ReviewForm
 
 # Create your views here.
 
+def get_users_viewable_reviews(request):
+    user_reviews = Review.objects.filter(user=request.user)
+    followings = UserFollow.objects.filter(followed_user=request.user)
+    for following in followings:
+        following_user_reviews = Review.objects.filter(user=following.user)
+        reviews =  user_reviews | following_user_reviews
+        return reviews
+
+
+def get_users_viewable_tickets(request):
+    user_tickets = Ticket.objects.filter(user=request.user)
+    followings = UserFollow.objects.filter(followed_user=request.user)
+    for following in followings:
+        following_user_tickets = Ticket.objects.filter(user=following.user)
+        tickets =  user_tickets | following_user_tickets
+        return tickets
+
+
+def get_responded_tickets(request):
+    reviews = get_users_viewable_reviews(request)
+    resolve_tickets_id = []
+    tickets_id = []
+    for review in reviews:
+        resolve_tickets = Ticket.objects.filter(id=review.ticket.id).values_list('pk', flat=True)
+        resolve_tickets_id.append(resolve_tickets)
+    for ticket_id in resolve_tickets_id:
+        tickets_id.append(ticket_id[0])
+    return tickets_id
+
+def get_wishlisted_reviews(request):
+    books = Book.objects.filter(user=request.user)
+    wishlisted_reviews_id = []
+    for book in books:
+        wishlisted_reviews_id.append(book.ticket_id)
+    return wishlisted_reviews_id
+
 @login_required
 def feed(request):
-    followings = UserFollow.objects.filter(followed_user=request.user)
-    books = Book.objects.all()
-    for following in followings:
-        tickets = Ticket.objects.filter(user=following.user)
-        tickets = tickets.annotate(content_type=Value("TICKET", CharField()))
-        reviews = Review.objects.filter(user=following.user)
-        reviews = reviews.annotate(content_type=Value("REVIEW", CharField()))
-        if tickets.exists() or reviews.exists():
-            resolve_ticket_id = [
-                Ticket.objects.filter(id=review.ticket.id) for review in reviews
-            ]
-            posts = sorted(
-                chain(tickets, reviews),
-                key=lambda post: post.time_created,
-                reverse=True,
-            )
-            return render(
-                request,
-                "feed/feed.html",
-                {
-                    "posts": posts,
-                    "resolve_ticket_id": resolve_ticket_id,
-                    "range": range(5),
-                    "books":books,
-                },
-            )
-        else:
-            message = "Il n'y a pas encore de critiques ou de tickets dans votre flux."
-            return render(
-                request,
-                "feed/feed.html",
-                {
-                    "message": message,
-                },
-            )
+    reviews = get_users_viewable_reviews(request)
+    reviews = reviews.annotate(content_type=Value("REVIEW", CharField()))
+    tickets = get_users_viewable_tickets(request)
+    tickets = tickets.annotate(content_type=Value("TICKET", CharField()))
+    if tickets.exists() or reviews.exists():
+        resolve_tickets = get_responded_tickets(request)
+        wishlisted_reviews = get_wishlisted_reviews(request)
+        print(wishlisted_reviews)
+        posts = sorted(
+            chain(tickets, reviews),
+            key=lambda post: post.time_created,
+            reverse=True,
+        )
+        return render(
+            request,
+            "feed/feed.html",
+            {
+                "posts": posts,
+                "resolve_tickets": resolve_tickets,
+                "range": range(5),
+                "wishlisted_reviews": wishlisted_reviews
+            },
+        )
+    else:
+        message = "Il n'y a pas encore de critiques ou de tickets dans votre flux."
+        return render(
+            request,
+            "feed/feed.html",
+            {
+                "message": message,
+            },
+        )
 
 
 @login_required
--- litreview_project/profil/models.py
+++ litreview_project/profil/models.py
@@ -9,5 +9,5 @@ class Book(models.Model):
 	summary = models.TextField(max_length=2048, blank=True)
 	image = models.ImageField(null=True, blank=True, upload_to="images/")
 	user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-	ticket_id = models.CharField(max_length=100, blank=True)
+	ticket_id = models.IntegerField(blank=True)
 	time_created = models.DateTimeField(auto_now_add=True)
